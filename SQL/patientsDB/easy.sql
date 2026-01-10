ed454bb7fcca3d32227e855b426a2eee4bec2f8f
Samarth1337 <samarth.saxena1337@gmail.com>
2026-01-10 17:13:10 +0530

    2026-01-10 easy sql prac

--- SQL/patientsDB/easy.sql
+++ SQL/patientsDB/easy.sql
@@ -13,3 +13,19 @@ SELECT first_name, last_name FROM patients WHERE weight BETWEEN 100 AND 120;
 
 -- Show unique birth years from patients and order them by ascending.
 SELECT distinct year(birth_date) FROM patients order by birth_date asc;
+
+-- Show first name and last name concatenated into one column to show their full name.
+SELECT concat(first_name," ", last_name) as full_name FROM patients;
+
+-- Show first name, last name, and the full province name of each patient. Example: 'Ontario' instead of 'ON'
+SELECT first_name, last_name, province_name FROM patients join province_names on province_names.province_id = patients.patient_id;
+
+-- Show how many patients have a birth_date with 2010 as the birth year.
+select COUNT(*) from patients where year(birth_date) = 2010;
+
+-- Show the first_name, last_name, and height of the patient with the greatest height.
+select first_name, last_name, MAX(height) from patients;
+
+-- Show all columns for patients who have one of the following patient_ids:
+-- 1,45,534,879,1000
+select * from patients where patient_id in (1,45,534,879,1000);
