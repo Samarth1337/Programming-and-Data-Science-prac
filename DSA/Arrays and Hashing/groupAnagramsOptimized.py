8845030b23caedcee87b70eb2de4144559021d41
Samarth Saxena <samarth.saxena1337@gmail.com>
2026-02-06 21:58:08 -0800

    2026-02-06 medium DSA group anagrams hashmap defaultdict(list) o(n)

---
+++ DSA/Arrays and Hashing/groupAnagramsOptimized.py
@@ -0,0 +1,48 @@
+# 49. Group Anagrams
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+#
+#
+#
+# Example 1:
+#
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+#
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+#
+# Explanation:
+#
+# There is no string in strs that can be rearranged to form "bat".
+# The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+# The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+# Example 2:
+#
+# Input: strs = [""]
+#
+# Output: [[""]]
+#
+# Example 3:
+#
+# Input: strs = ["a"]
+#
+# Output: [["a"]]
+#
+#
+#
+# Constraints:
+#
+# 1 <= strs.length <= 104
+# 0 <= strs[i].length <= 100
+# strs[i] consists of lowercase English letters.
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = defaultdict(list)
+        for s in strs:
+            sorted_key = "".join(sorted(s))
+            hash_map[sorted_key].append(s)
+        return list(hash_map.values())
