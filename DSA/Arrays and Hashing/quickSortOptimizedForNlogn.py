ba865127198b81a6905723e30db4515e4208df44
Samarth Saxena <samarth.saxena1337@gmail.com>
2026-02-19 23:25:15 -0800

    Updated problem descriptions for quick sort an array and quick sort colors (dutch flag problem)

--- DSA/Arrays and Hashing/quickSortOptimizedForNlogn.py
+++ DSA/Arrays and Hashing/quickSortOptimizedForNlogn.py
@@ -1,31 +1,29 @@
-# 912. Sort an Array
-# Solved
-# Medium
-# Topics
-# premium lock icon
-# Companies
-# Given an array of integers nums, sort the array in ascending order and return it.
+# Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 #
-# You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+#
+# You must solve this problem without using the library's sort function.
 #
 #
 #
 # Example 1:
 #
-# Input: nums = [5,2,3,1]
-# Output: [1,2,3,5]
-# Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+# Input: nums = [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
 # Example 2:
 #
-# Input: nums = [5,1,1,2,0,0]
-# Output: [0,0,1,1,2,5]
-# Explanation: Note that the values of nums are not necessarily unique.
+# Input: nums = [2,0,1]
+# Output: [0,1,2]
 #
 #
 # Constraints:
 #
-# 1 <= nums.length <= 5 * 104
-# -5 * 104 <= nums[i] <= 5 * 104
+# n == nums.length
+# 1 <= n <= 300
+# nums[i] is either 0, 1, or 2.
+#
+#
+# Follow up: Could you come up with a one-pass algorithm using only constant extra space?
 
 import random
 class Solution:
